4b2f091b8dff6d0fd896f6047c482af7851ca67e
jnim <jnimgaon@gmail.com>
2017-08-27 19:24:03 -0400

    about to implement seeding algorithm, then need to put in simulation algorithm

--- PlayoffDatePredictor.py
+++ PlayoffDatePredictor.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from helpers import AddTeams, StandingsUpdate
+from helpers import AddTeams, UpdateRecord, Seed, UpdateTiebreak, ElimFromPlayoffs, HasPlayed42Games
 
 #Here we make the object that will store the information for each team
 
@@ -10,29 +10,62 @@ from helpers import AddTeams, StandingsUpdate
 #'df' is short for DataFrame, which is pretty much a table. 
 dfRegSeason = pd.read_csv('RegularSeason.csv', encoding = 'ISO-8859-1')
 dfDivisionConfInfo = pd.read_csv('DivisionInfo.csv', encoding = 'ISO-8859-1')
+#print(dfRegSeason)
 
 #Now we track games through the regular season with a dataframe for the standings 
 #that we will update after each game - we start it by taking division/conference info
 #and adding some other columns that we will want to track like Wins, Losses, etc.
 dfStandings	= AddTeams(dfDivisionConfInfo)
 
+print(dfStandings)
+#We also make a 30x30 array that has the tiebreak results of each team. Each team
+#has an index value assigned below. Say a team with index i beats a team with index
+#j - then the matrix value at array(i, j) will be incremented by 1. If array(i, j)>
+#array(j, i), then team i won the season series. Otherwise, team j won. 
 
-for game in range (0, 1):
-#for game in range(len(dfRegSeason)):
+tiebreak = np.zeros((31, 31))
+TeamIndex = {'Boston Celtics': 1, 'Brooklyn Nets': 2, 'New York Knicks': 3, 'Philadelphia 76ers': 4, 'Toronto Raptors': 5, 'Chicago Bulls': 6, 'Cleveland Cavaliers': 7, 'Detroit Pistons': 8, 'Indiana Pacers': 9, 'Milwaukee Bucks': 10, 'Atlanta Hawks': 11, 'Charlotte Hornets': 12, 'Miami Heat': 13, 'Orlando Magic': 14, 'Washington Wizards': 15, 'Denver Nuggets': 16, 'Minnesota Timberwolves': 17, 'Oklahoma City Thunder': 18, 'Portland Trail Blazers': 19, 'Utah Jazz': 20, 'Golden State Warriors': 21, 'LA Clippers': 22, 'Los Angeles Lakers': 23, 'Phoenix Suns': 24, 'Sacramento Kings': 25, 'Dallas Mavericks': 26, 'Houston Rockets': 27, 'Memphis Grizzlies': 28, 'New Orleans Pelicans': 29, 'San Antonio Spurs': 30}
+
+bool = False
+for game in range(0, 576):
+for game in range(len(dfRegSeason)):
 
 	#Giving current game data names that are easier to read
 	HomeTeam = dfRegSeason['Home Team'][game]
 	AwayTeam = dfRegSeason['Away Team'][game]
 	WinningTeam = dfRegSeason['Winner'][game]
 	Date = dfRegSeason['Date'][game]
+	TeamsCanBeEliminated = HasPlayed42Games(dfStandings) #Unless a team has played 42 games, no team can be eliminated
+	
 
-	print('Home ' + str(HomeTeam) + ' Away ' + str(AwayTeam) + ' Winner ' + str(WinningTeam))
+	if not bool and TeamsCanBeEliminated:
+		print('game no: ' + str(game) + '\n')
+		bool = True
+
+	try: 
+		NextGameDate = dfRegSeason['Date'][game+1]
+	except(KeyError):
+		NextGameDate = 0 #We are on the last game, and we want to re-seed. Setting it to 0 ensures re-seeding
+
+	#print('Home ' + str(HomeTeam) + ' Away ' + str(AwayTeam) + ' Winner ' + str(WinningTeam))
 	
 	#print(dfRegSeason['Home Team'][game])
 
-	if WinningTeam == 'Home': #If winner of the currentgame is the Home team, then we report the Home team as the winner
-		StandingsUpdate(dfStandings, Date, HomeTeam, AwayTeam, dfRegSeason)
+	if WinningTeam == 'Home': #If winner of the current game is the Home team, then we report the Home team as the winner
+		tiebreak = UpdateTiebreak(tiebreak, HomeTeam, AwayTeam) #The season series is updated
+		dfStandings = UpdateRecord(dfStandings, HomeTeam, AwayTeam)
 
+		if (NextGameDate != Date and TeamsCanBeEliminated): #Only calculate standings after a full day of games - improves performance
+			#Neither of the lines below run unless a team has played 42 games - before that, no team can be eliminated from playoff contention
+			dfStandings = Seed(dfStandings, Date, dfRegSeason, tiebreak, TeamIndex, game) 
+			dfStandings = ElimFromPlayoffs(dfStandings, dfRegSeason, tiebreak, TeamIndex, game)
+	
 	else: #If winner of the currentgame is the Away team, then we report the away team as the winner
-		StandingsUpdate(dfStandings, Date, AwayTeam, HomeTeam, dfRegSeason)
+		tiebreak = UpdateTiebreak(tiebreak, AwayTeam, HomeTeam) #Season series is updated
+		dfStandings = UpdateRecord(dfStandings, AwayTeam, HomeTeam)
+
+		if (NextGameDate != Date and TeamsCanBeEliminated): #Only calculate standings after a full day of games - improves performance
+			NewSeeding(dfStandings, Date, dfRegSeason, tiebreak, TeamIndex, game)
+
+
 
--- helpers.py
+++ helpers.py
@@ -7,8 +7,64 @@ def AddTeams(dfStandings):
 	dfStandings['DivLeader'] = 0
 	dfStandings['Seed'] = 0
 	dfStandings['Date Elim'] = 0
+	return dfStandings
 
-
-def StandingsUpdate(TeamsInfo, DateofGame, TeamWon, TeamLost, RegSeasonSchedule):
-	return 0
+def UpdateRecord(dfStandings, TeamWon, TeamLost):
 	
+	Won = dfStandings.query('Team_Name == @TeamWon')['Wins']
+	dfStandings.loc[dfStandings['Team_Name'] == TeamWon, 'Wins'] = Won + 1
+	
+	#print(dfStandings.loc[dfStandings['Team_Name'] == TeamLost])
+	Lost = dfStandings.query('Team_Name == @TeamLost')['Losses']
+	dfStandings.loc[dfStandings['Team_Name'] == TeamLost, 'Losses'] = Lost + 1
+	
+	return dfStandings
+
+
+def Seed(dfStandings, Date, dfRegSeason, tiebreak, tiebreakIndex, gameIndex):
+	
+	dfStandings['W/L'] = dfStandings['Wins']/(dfStandings['Wins'] + dfStandings['Losses'])
+	ToBeSorted = dfStandings
+	
+	dfWest = dfStandings[dfStandings['Conference_id'] == 'West']
+	dfWest = dfWest.reset_index(drop = True)
+
+	dfEast = dfStandings[dfStandings['Conference_id'] == 'East']
+	dfEast = dfEast.reset_index(drop = True)
+	
+	Sorted = pd.DataFrame()
+	while(len(Sorted)<15): 
+		counter = 0
+		Highest = -1
+		while(counter!=len(dfWest)): #We go through the teams, find highest seed, then take it out and repeat
+			if Highest == -1: #If this is the first team we are examining
+				#print(dfWest)
+				Highest = dfWest[counter]
+
+	Sorted = pd.DataFrame()
+
+		dfNewStandings = pd.DataFrame()
+
+	#for team in range(len(dfStandings) - 1):
+
+
+	return dfStandings
+
+
+
+def ElimFromPlayoffs(dfStandings, dfRegSeason, arrTiebreak, tiebreakIndex, game):
+
+	return 0
+
+def HasPlayed42Games(dfStandings):
+	np.where(dfStandings['Wins'] + dfStandings['Losses'] >= 42, 1, 0)
+	Ans = 1 in np.where(dfStandings['Wins'] + dfStandings['Losses'] >= 42, 1, 0) 
+	return Ans
+
+def UpdateTiebreak(arrTiebreak, TeamWon, TeamLost):
+	TeamIndex = {'Boston Celtics': 1, 'Brooklyn Nets': 2, 'New York Knicks': 3, 'Philadelphia 76ers': 4, 'Toronto Raptors': 5, 'Chicago Bulls': 6, 'Cleveland Cavaliers': 7, 'Detroit Pistons': 8, 'Indiana Pacers': 9, 'Milwaukee Bucks': 10, 'Atlanta Hawks': 11, 'Charlotte Hornets': 12, 'Miami Heat': 13, 'Orlando Magic': 14, 'Washington Wizards': 15, 'Denver Nuggets': 16, 'Minnesota Timberwolves': 17, 'Oklahoma City Thunder': 18, 'Portland Trail Blazers': 19, 'Utah Jazz': 20, 'Golden State Warriors': 21, 'LA Clippers': 22, 'Los Angeles Lakers': 23, 'Phoenix Suns': 24, 'Sacramento Kings': 25, 'Dallas Mavericks': 26, 'Houston Rockets': 27, 'Memphis Grizzlies': 28, 'New Orleans Pelicans': 29, 'San Antonio Spurs': 30}
+	old_val = arrTiebreak[TeamIndex[TeamWon], TeamIndex[TeamLost]]
+	new_val = old_val + 1 #Update the season series
+	arrTiebreak[TeamIndex[TeamWon], TeamIndex[TeamLost]] = new_val
+
+	return arrTiebreak
